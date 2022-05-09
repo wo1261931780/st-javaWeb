8cc6f251e8191c1dd18060b5520c635b4976e74b
liujiajun <642344572@qq.com>
2022-05-09 21:01:56 +0800

    init

---
+++ src/main/java/com/stssm/github/io/junw/JunwApplication.java
@@ -0,0 +1,13 @@
+package com.stssm.github.io.junw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stssm/github/io/junw/JunwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stssm.github.io.junw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
