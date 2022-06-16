f71436de022e9da510c6040a18c42c32910a1f63
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-06-16 22:42:26 +0800

    Create aaa012demo.java

---
+++ src/main/java/com/stssm/github/io/javaweb/aaa004JDBC2022年6月16日/aaa012demo.java
@@ -0,0 +1,17 @@
+package com.stssm.github.io.javaweb.aaa004JDBC2022年6月16日;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:st-ssm.github.io
+ * Package:com.st-ssm.github.io.javaweb.aaa004JDBC2022年6月16日
+ * User:liujiajun_junw
+ * Time:2022-06-20-52  星期四
+ */
+public class aaa012demo {
+	public static void main(String[] args) {
+		// maven创造的目的，在于统一所有不同ide的项目结构
+		// 通过maven管理的项目，项目结构和层次都相同
+		// 通过唯一标识，让不同的项目之间相互依赖
+		
+	}
+}
