470ac10d79ae0faab134bee58493a05fd74bea9b
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-06-23 23:11:35 +0800

    day6-1

---
+++ src/main/java/com/stssm/github/io/javaweb/aaa005JDBC2022年6月23日/aaa022html.java
@@ -0,0 +1,19 @@
+package com.stssm.github.io.javaweb.aaa005JDBC2022年6月23日;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:st-ssm.github.io
+ * Package:com.stssm.github.io.javaweb.aaa005JDBC2022年6月23日
+ *
+ * @param
+ * @author liujiajun_junw
+ * @Date 2022-06-22-14  星期四
+ * @description
+ * @return
+ * @exception
+ */
+public class aaa022html {
+	public static void main(String[] args) {
+		
+	}
+}
