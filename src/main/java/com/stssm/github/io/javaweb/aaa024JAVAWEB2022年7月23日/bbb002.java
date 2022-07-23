f54a9d17f2f62e599a245a9b1c76924416248ca7
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-07-23 23:06:37 +0800

    day12-10

---
+++ src/main/java/com/stssm/github/io/javaweb/aaa024JAVAWEB2022年7月23日/bbb002.java
@@ -0,0 +1,32 @@
+package com.stssm.github.io.javaweb.aaa024JAVAWEB2022年7月23日;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:ajax-demo
+ * Package:com.itheima.web.servlet
+ *
+ * @param
+ * @author liujiajun_junw
+ * @Date 2022-07-20-46  星期六
+ * @description
+ * @return
+ * @exception
+ */
+public class bbb002 {
+	// 类似注册和搜索，都是同步的请求，
+	// 只要我使用了这个功能，那么页面就会根据我的要求去执行刷新等操作
+	// 异步请求是在客户界面上看不到的
+	// 但是后台会继续执行代码来获取数据
+
+
+
+
+
+
+
+
+
+
+
+
+}
