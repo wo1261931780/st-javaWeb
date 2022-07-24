d1649b5f1fd57bc5088c5cf2dd58126c6565277b
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-07-24 15:56:44 +0800

    day13-3

---
+++ src/main/java/com/stssm/github/io/javaweb/aaa025JAVAWEB2022年7月24日/DEMO.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+	name: "DEMO"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main/java/com/stssm/github/io/javaweb/aaa025JAVAWEB2022年7月24日/bbb003.java
+++ src/main/java/com/stssm/github/io/javaweb/aaa025JAVAWEB2022年7月24日/bbb003.java
@@ -9,20 +9,27 @@ import com.alibaba.fastjson.JSON;
  *
  * @param
  * @author liujiajun_junw
- * @Date 2022-07-10-58  星期日
+ * @Date 2022-07-10-58 星期日
  * @description
  * @return
  * @exception
  */
 public class bbb003 {
 	public static void main(String[] args) {
+		// json和js的格式类似
+		// 但是json的键值对，必须使用大括号来表示
+		// 我们在客户端和服务器，中间是使用json来传递数据，
+		// 在服务端，需要对json进行解析，然后将其变成一个java对象进行后续的数据操作
+		// 同理，服务器对数据的响应，需要将java对象封为一个json格式的字符数据
+		// 这里直接使用开源的json格式化工具
 		brandPojo brandTest = new brandPojo();
 		brandTest.setAge(11);
 		brandTest.setAddress("1111111");
 		brandTest.setName("22222222222");
 		String s = JSON.toJSONString(brandTest);
 		System.out.println(s);// {"address":"1111111","age":11,"name":"22222222222"}
-		brandPojo brandPojo = JSON.parseObject("{\"address\":\"1111111\",\"age\":11,\"name\":\"22222222222\"}", brandPojo.class);
+		brandPojo brandPojo = JSON.parseObject("{\"address\":\"1111111\",\"age\":11,\"name\":\"22222222222\"}",
+				brandPojo.class);
 		System.out.println(brandPojo);// brandPojo{name='22222222222', age=11, address='1111111'}
 		System.out.println("我是对象内部的属性：" + brandPojo.getName());// 我是对象内部的属性：22222222222
 
