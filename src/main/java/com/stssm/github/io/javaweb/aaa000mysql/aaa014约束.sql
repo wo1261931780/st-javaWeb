94916274af3723561bffde21f0abd3874b2cfd0e
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-08-09 21:07:08 +0800

    Update aaa014约束.sql

--- src/main/java/com/stssm/github/io/javaweb/aaa000mysql/aaa014约束.sql
+++ src/main/java/com/stssm/github/io/javaweb/aaa000mysql/aaa014约束.sql
@@ -1,6 +1,24 @@
+# 1.约束的概念
+# 约束是作用于表中列上的规则，用于限制加入表的数据
+# 约束的存在保证了数据库中数据的正确性、有效性和完整性
+#
+# 2.约束的分类
+# 非空约束 保证列中所有数据不能有null值 NOT NULL
+#
+# 唯一约束 保证列中所有数据各不相同 UNIQUE
+#
+# 主键约束 主键是一行数据的唯一标识，要求非空且唯一 PRIMARYKEY
+#
+# 检查约束 保证列中的值满足某一条件 CHECK
+#
+# 	默认约束 保存数据时，未指定值则采用默认值 DEFAULT
+#
+# 外键约束 外键用来让两个表的数据之间建立链接，保证数据的一致性和完整性 FOREIGNKEY
+#
+# Tips：MySQL不支持检查约束
+#########################################################################
+
 SELECT * FROM demo1;
-
-
 
 DROP TABLE if EXISTS demo2;
 -- SQL的要求是非常严格的
@@ -10,19 +28,30 @@ DROP TABLE if EXISTS demo2;
 CREATE TABLE demo2 (
 id INT PRIMARY KEY AUTO_INCREMENT,-- 主键，默认非空且唯一
 -- 自增长必须是数字类型的元素才可以使用
-ename VARCHAR(50) NOT NULL UNIQUE,
-takeDt DATE NOT NULL,
-salary DOUBLE(5,2) NOT NULL,
-bonus DOUBLE(5,2) DEFAULT 0
+e_name VARCHAR(50) NOT NULL UNIQUE, -- 非空且唯一
+takeDt DATE NOT NULL, -- 非空
+salary DOUBLE(5,2) NOT NULL, -- 非空
+bonus DOUBLE(5,2) DEFAULT 0 -- 默认为0
 );
 
+-- 也可以建完表以后添加非空约束
+alter table demo1 modify name VARCHAR(50) NOT NULL;
+-- 删除约束条件
+alter table demo1 modify name VARCHAR(50);
+
+#########################################################################
+
+
+
+
+
 SELECT * FROM DEMO2;
-INSERT INTO DEMO2(id,ename,takeDt,salary,bonus) VALUES('1','2','2020-01-01','4','5');
+INSERT INTO DEMO2(id,e_name,takeDt,salary,bonus) VALUES('1','2','2020-01-01','4','5');
 -- 对于日期类型，只要是按照固定格式写入，就可以进行解析
-INSERT INTO DEMO2(id,ename,takeDt,salary,bonus) VALUES('2','3','20200101','4','5');
+INSERT INTO DEMO2(id,e_name,takeDt,salary,bonus) VALUES('2','3','20200101','4','5');
 -- 直接插入会报错
 -- > 1048 - Column 'id' cannot be null
-INSERT INTO DEMO2(id,ename,takeDt,salary,bonus) VALUES(NULL,'4','20200101','4','5');
+INSERT INTO DEMO2(id,e_name,takeDt,salary,bonus) VALUES(NULL,'4','20200101','4','5');
 
 
 DROP table  if EXISTS demo3emp;
