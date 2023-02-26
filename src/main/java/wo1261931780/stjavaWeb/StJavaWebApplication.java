08b671c28ef7d46fdfe224151695ed3bc1365e82
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-26 21:02:30 +0800

    2

---
+++ src/main/java/wo1261931780/stjavaWeb/StJavaWebApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.stjavaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StJavaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/stjavaWeb/StJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931780.stjavaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
